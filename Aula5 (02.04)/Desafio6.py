cdaabf184fa819316074a74893457aa83c35dd29
okamoto-amanda <amanda.okam@gmail.com>
2024-04-04 13:18:48 -0300

    ok

--- Aula5 (02.04)/Desafio6.py
+++ Aula5 (02.04)/Desafio6.py
@@ -2,9 +2,10 @@
 # de uma PA. No final, mostre os 10 primeiros termos dessa
 # progressão.
 
+resultado = []
 termo = int(input("Por favor, digite o primeiro termo da Progressão: "))
 razao = int(input("Por favor, digite a razão: "))
 for i in range(10):
-    termo + (i+1) * razao
-    
-print(i)
+    termos = termo + (i+1) * razao
+    resultado.append(termos)
+print(resultado)
