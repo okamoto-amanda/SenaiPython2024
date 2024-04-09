5f0b6328f486d1206342dd6bf1324475c188e9cd
okamoto-amanda <amanda.okam@gmail.com>
2024-04-09 15:43:06 -0300

    OK

--- Aula7/Desafio5.py
+++ Aula7/Desafio5.py
@@ -3,10 +3,12 @@
 # No final, mostre uma listagem de preços, organizando os dados em
 # forma tabular
 
-itens = ['Arroz',26, 'Feijão',8, 'Macarrão',3, 'Banana',4]
+itens = ('Arroz',26, 'Feijão',8, 'Macarrão',3, 'Banana',40)
 print(itens)
 print(len(itens))
 
 for i in range (0, len(itens)):
     if type(itens[i]) is str:
         print(f"Produto: {itens[i]}....Preço: R${itens[i+1]},00")
+        
+print(type(itens))
