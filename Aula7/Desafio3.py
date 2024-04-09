17879bb8af008c6ed14ba39163b9cb70e191a76d
okamoto-amanda <amanda.okam@gmail.com>
2024-04-09 16:07:02 -0300

    OK

--- Aula7/Desafio3.py
+++ Aula7/Desafio3.py
@@ -1,16 +1,16 @@
 
 timesBrasileirao = 'Amazonas','América-MG','Avaí','Botafogo-RP','Brusque','CRB','Ceará','Chapecoense','Coritiba','Goiás','Guarani','Ituano','Mirassol','Novorizontino','Operário-PR','Paysandu','Ponte Preta','Santos','Sport','Vila Nova'
 
-# Apenas os 5 primeiros colocados.
-# B) Os últimos 4 colocados da tabela.
-# C) Uma lista com os times em ordem alfabética.
-# D) Em que posição na tabela está o time do Santos.
+# Apenas os 5 primeiros colocados
+print(timesBrasileirao[:5])
 
-print(timesBrasileirao[5])
+#Os últimos 4 colocados da tabela
 print(timesBrasileirao[16:])
 
-ordemTimes = timesBrasileirao
+# Uma lista com os times em ordem alfabética.
 ordemTimes = sorted(timesBrasileirao)
 print(ordemTimes)
 
-
+# Em que posição na tabela está o time do Santos.
+posicaoSantos = timesBrasileirao.index('Santos')
+print(posicaoSantos)
