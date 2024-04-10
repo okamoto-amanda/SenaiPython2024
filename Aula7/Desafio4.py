6763a53d903859ff249ed171505d92ab5c253a0c
okamoto-amanda <amanda.okam@gmail.com>
2024-04-10 15:54:19 -0300

    OK

--- Aula7/Desafio4.py
+++ Aula7/Desafio4.py
@@ -22,3 +22,23 @@ for i in tuplaNumeroUsuario:
     if i % 2 == 0:
         numerosPares.append(i)
 print(f'Os numeros pares são:{numerosPares}')
+
+
+##OU#######################################################
+numerosUsuario2 = (int(input('Digite um número:'))), (int(input('Digite um número:'))), (int(input('Digite um número:'))), (int(input('Digite um número:')))                 
+
+# Quantas vezes apareceu o valor 9.
+print(f'O numero 9 apareceu {numerosUsuario2.count(9)} vezes')
+
+# Em que posição foi digitado o primeiro valor 3
+if 3 in numerosUsuario2:
+    print(f'O numero 3 apareceu na {numerosUsuario2.index(3)+1}ª posição')
+else:
+     print('O numero 3 apareceu não aparece na lista')
+
+# Quais foram o números pares.
+print(f'Os numeros pares são: ', end='')
+for i in numerosUsuario2:
+    if i % 2 == 0:
+        print(i)
+        
