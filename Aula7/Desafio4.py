264cca3db468df0dcc05c117ef6ed9486b34a258
okamoto-amanda <amanda.okam@gmail.com>
2024-04-10 15:09:24 -0300

    OK

--- Aula7/Desafio4.py
+++ Aula7/Desafio4.py
@@ -1,5 +1,7 @@
 
 numerosUsuario = []
+numerosPares = []
+
 while len(numerosUsuario) < 4:
     numerosUsuario.append(int(input("Por favor digite um número: ")))
 
@@ -7,12 +9,13 @@ tuplaNumeroUsuario = tuple(numerosUsuario)
 print(numerosUsuario)
 
 # Quantas vezes apareceu o valor 9.
-print(tuplaNumeroUsuario.count(9))
+print(f'O numero 9 apareceu {tuplaNumeroUsuario.count(9)} vezes')
 
 # Em que posição foi digitado o primeiro valor 3
-print(tuplaNumeroUsuario.index(3))
+print(f'O numero 3 apareceu na {tuplaNumeroUsuario.index(3)}ª posição')
 
 # Quais foram o números pares.
 for i in tuplaNumeroUsuario:
     if i % 2 == 0:
-        print(i)
+        numerosPares.append(i)
+print(f'Os numeros pares são:{numerosPares}')
