0d84cb504be331a9321dabcc6cf6c0db83be819f
okamoto-amanda <amanda.okam@gmail.com>
2024-04-11 16:32:04 -0300

    OK

---
+++ Aula9/ExemploDicionarios.py
@@ -0,0 +1,14 @@
+
+personagens = [
+    {"Nome":"Bob",
+     'Sobrenome':'Esponja',
+     'Idade':'10'
+    },
+    {'Nome':'Jhonny',
+     'Sobrenome':'Bravo',
+     'Idade':'15'
+    }
+    
+]
+
+print(personagens)
