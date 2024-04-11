103b2a81d86ef4a1194fd1290f28ad9107312c80
okamoto-amanda <amanda.okam@gmail.com>
2024-04-11 14:42:36 -0300

    OK

--- Aula8/Desafio3.py
+++ Aula8/Desafio3.py
@@ -5,7 +5,41 @@
 # No final mostre a lista ordenada na tela
 
 numeroUsuario = []
-while len(numeroUsuario) < 5:
-    numeroUsuario.append(int(input('Numero: ')))
 
+for i in range (0,5):
+    n = int(input('Digite um numero: '))
+    
+    if i == 0 or n > numeroUsuario[-1]:
+        numeroUsuario.append(n)
+        print("Adicionado ao final da lista")
+        
+    else:
+        pos = 0
+        while pos < len(numeroUsuario):
+            if n <= numeroUsuario[pos]:
+                numeroUsuario.insert(pos,n)
+                print(f'Adicionado na posição {pos} da lista')
+                break
+        pos += 1
+print(f"Os valores da lista são {numeroUsuario}")
 #nao finalizado
+
+
+# lista = [] #10
+
+# for c in range(0,5):    
+#     n = int(input("Digite um valor: ")) #10 ,1, 15,20,7   c=2
+
+#     if c == 0 or n > lista[-1]:
+#         lista.append(n)
+#         print('Adicionado ao final da lista')
+#     else:
+#         pos = 0
+#         while pos < len(lista):  # 2<2
+#             if n <= lista[pos]: # 15<10
+#                 lista.insert(pos,n)# lista = [1,10,15,20]
+#                 print(f"Adicionado na posição {pos} da lista")
+#                 break
+#             pos += 1
+
+# print(f"Os valores digitados em ordem foram {lista}")
