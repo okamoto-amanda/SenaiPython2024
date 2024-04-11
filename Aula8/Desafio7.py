aaee9fceeb51a23612cc618a3d831436c97ff2c7
okamoto-amanda <amanda.okam@gmail.com>
2024-04-11 16:10:52 -0300

    OK

--- Aula8/Desafio7.py
+++ Aula8/Desafio7.py
@@ -2,4 +2,21 @@
 # preencha com valores lidos pelo teclado.
 # No final mostre a Matriz na tela, com a formatação correta.
 
+matriz = [[0,0,0],[0,0,0],[0,0,0]]
+c = 0
+
+for i in range(0,3):
+    for j in matriz[i]:
+        
+        if c == 3:
+            c = 0
+        
+        numero = int(input(f"Digite um valor para a posição {i},{c}: "))
+        matriz[i][c] = numero
+            
+        c += 1
+print(matriz)       
+for i in range(0,3):
+    print(matriz[i])
+    i += 1
 
