5dac226579c37ab1580d3cfc66786f6f61ce9f7d
okamoto-amanda <amanda.okam@gmail.com>
2024-04-22 13:59:32 -0300

    OK

---
+++ Aula10 - Funcoes/Exemplosfuncoes.py
@@ -0,0 +1,11 @@
+def calculaArea (largura, altura):
+    area = largura * altura
+    print(area)
+    
+def calculoPerimetro (largura, altura):
+    perimetro = (2 * largura) + (2 * altura)
+    return perimetro
+
+
+print(calculaArea(5, 3))
+print(calculoPerimetro (5, 3))
