a14382350f7a366cb1817905e93d3897c25ae47a
okamoto-amanda <amanda.okam@gmail.com>
2024-04-22 16:07:29 -0300

    OK

--- Aula10 - Funcoes/Desafio4.py
+++ Aula10 - Funcoes/Desafio4.py
@@ -3,10 +3,12 @@
 # Seu programa tem que analisar todos os valores e dizer qual deles é
 # o maior.
 
-
+a = int(input("Digite um valor: "))
+b = int(input("Digite um valor: "))
+c = int(input("Digite um valor: "))
+    
 def maior(a, b, c):
-    a = int(input("Digite um valor: "))
-    b = int(input("Digite um valor: "))
-    c = int(input("Digite um valor: "))
-    lista = []
+    valores = [a,b,c]
+    print(f"O maior número é o {max(valores)}")
 
+maior(a,b,c)
