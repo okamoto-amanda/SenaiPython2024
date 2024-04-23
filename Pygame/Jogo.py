5887c4f8194f437ae23137811ca75f81eb2b55dd
okamoto-amanda <amanda.okam@gmail.com>
2024-04-23 15:55:51 -0300

    OK

--- Pygame/Jogo.py
+++ Pygame/Jogo.py
@@ -1,5 +1,27 @@
-from pygame import *
+import pygame
 from pygame.locals import *
-from sys import exit
+from sys import *
 
 pygame.init()
+
+altura = 480
+largura = 640
+
+#criando a janela
+tela = pygame.display.set_mode((largura, altura))
+
+#Definir titulo da janela
+pygame.display.set_caption("Criando Jogos")
+
+while True:
+    
+    #colocando eventos
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            exit()
+    
+    #Atualizar o jogo a cada interação
+    pygame.display.update()
+    
+    pygame.draw.rect(tela)
